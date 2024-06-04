e97ab63720bee70c1688236b69293f8ae252b0c1
Alex Böhm <tools@boehm.work>
2024-06-04 15:48:05 +0200

    refactor(studio): harden env variable handling

--- apps/studio/sanity.cli.ts
+++ apps/studio/sanity.cli.ts
@@ -1,10 +1,10 @@
-/* eslint-disable node/prefer-global/process */
-
 import { defineCliConfig } from 'sanity/cli';
+
+import { dataset, projectId } from './env';
 
 export default defineCliConfig({
 	api: {
-		dataset: process.env.SANITY_STUDIO_DATASET,
-		projectId: process.env.SANITY_STUDIO_PROJECT_ID,
+		dataset,
+		projectId,
 	},
 });
