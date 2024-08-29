5f1a08a4bbb88d88850737738672956189c9cb61
Alex Böhm <tools@boehm.work>
2024-08-29 12:08:28 +0200

    feat(web): add initial configuration

--- apps/web/astro.config.ts
+++ apps/web/astro.config.ts
@@ -1,4 +1,23 @@
+import react from '@astrojs/react';
+import tailwind from '@astrojs/tailwind';
+import vercel from '@astrojs/vercel/serverless';
+import sanity from '@sanity/astro';
 import { defineConfig } from 'astro/config';
 
+import { apiVersion, dataset, projectId } from './src/env';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	adapter: vercel(),
+	integrations: [
+		tailwind(),
+		react(),
+		sanity({
+			apiVersion,
+			dataset,
+			projectId,
+			useCdn: true,
+		}),
+	],
+	output: 'server',
+});
