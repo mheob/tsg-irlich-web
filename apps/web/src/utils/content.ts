37eee575ca6f95d58cdd84b7439e6f269d96932a
Alex Böhm <tools@boehm.work>
2024-09-02 20:57:51 +0200

    feat(web): add feature section
    closes #74

--- apps/web/src/utils/content.ts
+++ apps/web/src/utils/content.ts
@@ -1,10 +1,11 @@
 /**
- * Checks if the given content is an HTML string.
+ * Checks if the given content is valid HTML.
  *
- * @param content - The string to check.
- * @returns `true` if the content is an HTML string, `false` otherwise.
+ * @param content - The content to check.
+ * @returns `true` if the content is valid HTML, `false` otherwise.
  */
-export function isHTML(content: string): boolean {
+export function isHTML(content?: string): boolean {
+	if (!content) return false;
 	content = content.trim();
 	return content.startsWith('<') && content.endsWith('>');
 }
