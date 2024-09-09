0ef8956bf319ceb50b14d940ad75e13b13b4bc36
Alex Böhm <tools@boehm.work>
2024-09-09 19:51:10 +0200

    refactor(web): use `index.ts` files to simplify imports

---
+++ apps/web/src/components/items/buttons/index.ts
@@ -0,0 +1,3 @@
+export { default as ArrowButton } from './ArrowButton.astro';
+export { default as ArrowButtonGroup } from './ArrowButtonGroup.astro';
+export { default as CtaButton } from './CtaButton.astro';
---
+++ apps/web/src/components/items/index.ts
@@ -0,0 +1,2 @@
+export * from './buttons';
+export { default as SectionHeader } from './SectionHeader.astro';
