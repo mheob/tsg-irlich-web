6c7049e4df8a69b3e72c110f0b933d1a85c5d281
Alex Böhm <tools@boehm.work>
2024-11-26 19:09:16 +0100

    fix(studio): use dashes instead of dots in `news-article-page`

--- apps/studio/schemas/single-pages/news-article.ts
+++ apps/studio/schemas/single-pages/news-article.ts
@@ -1,24 +1,20 @@
 import { RiBookletLine } from 'react-icons/ri';
 import { defineType } from 'sanity';
 
-import { content, general } from '@/shared/field-groups';
-import { contactPersonsField } from '@/shared/fields/contact';
+import { general } from '@/shared/field-groups';
 import { defaultPageFields } from '@/shared/fields/general';
 
 const newsArticlePage = defineType({
 	title: 'Einzelner News-Artikel',
-	name: 'news.article.page',
+	name: 'news-article-page',
 	type: 'document',
 	icon: RiBookletLine,
-	groups: [general, content],
+	groups: [general],
 	fields: [
 		// ?: the "slug" and `meta` comes from the news article itself; this page is rather the layout
 
 		// general
 		...defaultPageFields,
-
-		// content
-		contactPersonsField,
 	],
 	preview: {
 		prepare: () => ({ title: 'Einzelner News-Artikel' }),
