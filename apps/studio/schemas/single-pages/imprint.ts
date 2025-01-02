3a667ed2d6d7d6dfc9397342d6dc1fd2209a1c90
Alex Böhm <tools@boehm.work>
2025-01-02 08:49:33 +0100

    feat(studio): add imprint content

--- apps/studio/schemas/single-pages/imprint.ts
+++ apps/studio/schemas/single-pages/imprint.ts
@@ -3,10 +3,9 @@ import { RiSettings5Line } from 'react-icons/ri';
 import { defineField, defineType } from 'sanity';
 
 import { content, general, meta } from '@/shared/field-groups';
-import { addressField, emailField, phoneField } from '@/shared/fields/contact';
+import { addressField, emailField } from '@/shared/fields/contact';
 import { defaultPageFields, getHiddenSlugField } from '@/shared/fields/general';
 import { metaField } from '@/shared/fields/meta';
-import { phoneFieldRegexRule } from '@/shared/validation-rules';
 
 const imprintPage = defineType({
 	title: 'Impressum',
@@ -30,13 +29,6 @@ const imprintPage = defineType({
 			group: 'content',
 		}),
 		defineField({
-			title: 'Registernummer',
-			name: 'registerNo',
-			type: 'string',
-			group: 'content',
-			// validation: rule => [requiredRule(rule, 'Die Registernummer')],
-		}),
-		defineField({
 			title: 'Registergericht',
 			name: 'registerCourt',
 			type: 'string',
@@ -44,44 +36,41 @@ const imprintPage = defineType({
 			// validation: rule => [requiredRule(rule, 'Das Registergericht')],
 		}),
 		defineField({
-			title: 'Vertreten durch',
-			name: 'represented',
+			title: 'Registernummer',
+			name: 'registerNo',
 			type: 'string',
 			group: 'content',
-			// validation: rule => [requiredRule(rule, 'Das Feld "Vertreten durch"')],
+			// validation: rule => [requiredRule(rule, 'Die Registernummer')],
 		}),
 		defineField({
-			...phoneField,
+			title: 'Vertreten durch',
+			name: 'represented',
+			type: 'text',
 			group: 'content',
-			validation: rule => [
-				// requiredRule(rule, 'Telefon'),
-				phoneFieldRegexRule(rule),
-			],
+			// validation: rule => [requiredRule(rule, 'Das Feld "Vertreten durch"')],
 		}),
 		defineField({
 			...emailField,
 			group: 'content',
 		}),
 		defineField({
+			title: 'Kontaktformular',
+			name: 'contactForm',
+			type: 'string',
+			group: 'content',
+			// validation: rule => [requiredRule(rule, 'Telefon')],
+		}),
+		defineField({
 			title: 'Redaktionell verantwortlich',
 			name: 'responsible',
-			type: 'text',
+			type: 'string',
 			group: 'content',
 			// validation: rule => [requiredRule(rule, 'Das Feld "Redaktionell verantwortlich')],
 		}),
 		defineField({
-			title: 'Verbraucherstreitbeilegung / Universalschlichtungsstelle',
-			name: 'arbitrationBoard',
-			type: 'string',
-			group: 'content',
-			// validation: rule => [
-			// 	requiredRule(rule, '"Verbraucherstreitbeilegung / Universalschlichtungsstelle"'),
-			// ],
-		}),
-		defineField({
 			title: 'Name Technischer Ansprechpartner',
 			name: 'technicalQuestionsName',
-			type: 'text',
+			type: 'string',
 			group: 'content',
 			// validation: rule => [requiredRule(rule, 'Das Feld "Name Technischer Ansprechpartner"')],
 		}),
@@ -93,9 +82,23 @@ const imprintPage = defineType({
 			// validation: rule => [requiredRule(rule, 'Das Feld "E-Mail Technischer Ansprechpartner"')],
 		}),
 		defineField({
+			title: 'Social Media',
+			name: 'socialMedia',
+			type: 'text',
+			group: 'content',
+			// validation: rule => [requiredRule(rule, 'Das Feld "Social Media"')],
+		}),
+		defineField({
 			title: 'Freundliche Unterstützung durch',
 			name: 'support',
 			type: 'simpleBlockContent',
+			group: 'content',
+			// validation: rule => [requiredRule(rule, 'Das Feld "Freundliche Unterstützung durch"')],
+		}),
+		defineField({
+			title: 'Bildnachweise',
+			name: 'credits',
+			type: 'text',
 			group: 'content',
 			// validation: rule => [requiredRule(rule, 'Das Feld "Freundliche Unterstützung durch"')],
 		}),
